67280803571ec16145b2ae9c2fa083aa51cba9b9
Jun0se7en <93256752+Jun0se7en@users.noreply.github.com>
2024-08-08 01:40:37 +0700

    Update hao.py

--- recognition/arcface_torch/configs/hao.py
+++ recognition/arcface_torch/configs/hao.py
@@ -6,7 +6,7 @@ from easydict import EasyDict as edict
 
 config = edict()
 config.margin_list = (1.0, 0.0, 0.4)
-config.network = "mbf"
+config.network = "r50"
 config.resume = False
 config.output = "/kaggle/working/"
 config.embedding_size = 512
